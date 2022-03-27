0f9648c7a29b06f47737c086e13800d96e172a17
sanya-shch <shchipkov.sanya@ukr.net>
2022-03-27 20:34:58 +0300

    add class Layer

--- 2_Backgrounds/script.js
+++ 2_Backgrounds/script.js
@@ -15,9 +15,35 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-let gameSpeed = 5;
+let gameSpeed = 10;
 let x1 = 0;
 let x2 = 2400;
+
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update() {
+        this.speed = gameSpeed * this.speedModifier;
+
+        if (this.x < -2400) this.x = this.width + this.x2 - this.speed;
+        if (this.x2 < -2400) this.x2 = this.width + this.x - this.speed;
+
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
 
 function animate() {
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
