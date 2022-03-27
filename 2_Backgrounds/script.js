7927cc2c6485cf7e05b25d4f49b60eed8ffeff69
sanya-shch <shchipkov.sanya@ukr.net>
2022-03-27 20:19:10 +0300

    create endlessly scrolling backgrounds

---
+++ 2_Backgrounds/script.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'layer-5.png';
+
+let gameSpeed = 5;
+let x1 = 0;
+let x2 = 2400;
+
+function animate() {
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+
+    ctx.drawImage(backgroundLayer4, x1, 0);
+    ctx.drawImage(backgroundLayer4, x2, 0);
+
+    if (x1 < -2400) x1 = 2400 + x2 - gameSpeed;
+    else x1-=gameSpeed;
+    if (x2 < -2400) x2 = 2400 + x1 - gameSpeed;
+    else x2-=gameSpeed;
+
+    requestAnimationFrame(animate);
+}
+animate();
