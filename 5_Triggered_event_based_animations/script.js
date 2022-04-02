f7d78e83a1f2c865cd407ec9257d86043785fa98
sanya-shch <shchipkov.sanya@ukr.net>
2022-04-03 01:43:20 +0300

    add rotation and audio

--- 5_Triggered_event_based_animations/script.js
+++ 5_Triggered_event_based_animations/script.js
@@ -14,14 +14,19 @@ class Explosion {
         this.image.src = 'boom.png';
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
-        this.x = x - this.width / 2;
-        this.y = y - this.height / 2;
+        this.width = this.spriteWidth / 0.7;
+        this.height = this.spriteHeight / 0.7;
+        this.x = x;
+        this.y = y;
         this.frame = 0;
         this.speed = 10;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = 'boom.wav';
     }
     update() {
+        if (this.frame === 0) this.sound.play();
+
         if (gameFrame % this.speed === 0) {
             // this.frame > 4 ? this.frame = 0 : this.frame++;
 
@@ -29,28 +34,21 @@ class Explosion {
         }
     }
     draw() {
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 0 - this.width / 2, 0 - this.height / 2, this.width, this.height);
+        ctx.restore();
     }
 }
-
-// window.addEventListener('click', function (e) {
-//     // ctx.fillStyle = "white";
-//     // ctx.fillRect(e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50);
-//
-//     let positionX = e.x - canvasPosition.left;
-//     let positionY = e.y - canvasPosition.top;
-//
-//     explosions.push(new Explosion(positionX, positionY));
-//     console.log(explosions)
-// });
 
 window.addEventListener('click', function (e) {
     createAnimation(e);
 });
 
-window.addEventListener('mousemove', function (e) {
-    createAnimation(e);
-});
+// window.addEventListener('mousemove', function (e) {
+//     createAnimation(e);
+// });
 
 function createAnimation(e) {
     let positionX = e.x - canvasPosition.left;
