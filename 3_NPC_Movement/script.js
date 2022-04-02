cc384bc62f754141b198d68cdf79dd6a5aa26edb
sanya-shch <shchipkov.sanya@ukr.net>
2022-04-02 20:40:47 +0300

    add enemy movement type 2

--- 3_NPC_Movement/script.js
+++ 3_NPC_Movement/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
 
-const numberOfEnemies = 100;
+const numberOfEnemies = 10;
 const enemiesArray = [];
 let gameFrame = 0;
 
@@ -17,13 +17,44 @@ let gameFrame = 0;
 // const enemy4Image = new Image();
 // enemy4Image.src = 'enemy4.png';
 
+// // movement type 1
+// class Enemy {
+//     constructor() {
+//         this.image = new Image();
+//         this.image.src = 'enemy1.png';
+//         // this.speed = Math.random() * 4 - 2;
+//         this.spriteWidth = 293;
+//         this.spriteHeight = 155;
+//         this.width = this.spriteWidth / 2.5;
+//         this.height = this.spriteHeight / 2.5;
+//         this.x = Math.random() * (canvas.width - this.width);
+//         this.y = Math.random() * (canvas.height - this.height);
+//         this.frame = 1;
+//         this.flapSpeed = Math.floor(Math.random() * 3 +1);
+//     }
+//     update() {
+//         this.x += Math.random() * 5 - 2.5;
+//         this.y += Math.random() * 5 - 2.5;
+//
+//         if (gameFrame % this.flapSpeed === 0) {
+//             this.frame > 4 ? this.frame = 0 : this.frame++;
+//         }
+//     }
+//     draw() {
+//         // ctx.fillRect(this.x, this.y, this.width, this.height);
+//         // ctx.strokeRect(this.x, this.y, this.width, this.height);
+//         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+//     }
+// }
+
+// movement type 2
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = 'enemy1.png';
-        // this.speed = Math.random() * 4 - 2;
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        this.image.src = 'enemy2.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -32,16 +63,16 @@ class Enemy {
         this.flapSpeed = Math.floor(Math.random() * 3 +1);
     }
     update() {
-        this.x += Math.random() * 5 - 2.5;
-        this.y += Math.random() * 5 - 2.5;
+        this.x -= this.speed;
+        // this.y += Math.random() * 5 - 2.5;
+
+        if (this.x + this.width < 0) this.x = canvas.width;
 
         if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw() {
-        // ctx.fillRect(this.x, this.y, this.width, this.height);
-        // ctx.strokeRect(this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
