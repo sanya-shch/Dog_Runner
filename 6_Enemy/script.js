3ce2a0ea3ed2494d270c59324b5f27c2f815370f
sanya-shch <shchipkov.sanya@ukr.net>
2022-05-21 17:32:33 +0300

    added frame animation

--- 6_Enemy/script.js
+++ 6_Enemy/script.js
@@ -47,16 +47,29 @@ document.addEventListener("DOMContentLoaded", function () {
         constructor (game) {
             this.game = game;
             this.markedForDeletion = false;
+            this.frameX = 0;
+            this.maxFrame = 5;
+            this.frameInterval = 100;
+            this.frameTimer = 0;
         }
         update (deltaTime) {
             this.x -= this.vx * deltaTime;
 
             if (this.x < 0 - this.width) this.markedForDeletion = true;
-            if (this.y < 0 - this.height) this.markedForDeletion = true;
+            // if (this.y < 0 - this.height) this.markedForDeletion = true;
+
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX < this.maxFrame) this.frameX++;
+                else this.frameX = 0;
+
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
         }
         draw (ctx) {
             // ctx.fillRect(this.x, this.y, this.width, this.height);
-            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
 
@@ -120,6 +133,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         update(deltaTime) {
             super.update(deltaTime);
+
+            if (this.y < 0 - this.height * 2) this.markedForDeletion = true;
+
             this.y += this.vy * deltaTime;
 
             if(this.y > this.maxLength) this.vy *= -1;
