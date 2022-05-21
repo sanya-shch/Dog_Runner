0d8d3c543d301d0abcaa7dca0dac70353edc86fc
sanya-shch <shchipkov.sanya@ukr.net>
2022-05-21 15:51:18 +0300

    add Enemy class

---
+++ 6_Enemy/script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+    const CANVAS_WIDTH = canvas.width = 500;
+    const CANVAS_HEIGHT = canvas.height = 800;
+
+    class Game {
+        constructor (ctx, width, height) {
+            this.ctx = ctx;
+            this.width = width;
+            this.height = height;
+
+            this.enemies = [];
+
+            this.enemyInterval = 1000;
+            this.enemyTimer = 0;
+        }
+        update (deltaTime) {
+            if (this.enemyTimer > this.enemyInterval) {
+                this.#addNewEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+
+            this.enemies.forEach(obj => obj.update());
+        }
+        draw () {
+            this.enemies.forEach(obj => obj.draw(this.ctx));
+        }
+        #addNewEnemy () {
+            this.enemies.push(new Enemy(this));
+        }
+    }
+
+    class Enemy {
+        constructor (game) {
+            this.game = game;
+            this.x = this.game.width;
+            this.y = Math.random() * this.game.height;
+            this.width = 100;
+            this.height = 100;
+        }
+        update () {
+            this.x--;
+        }
+        draw (ctx) {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    const game = new Game(ctx, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let lastTime = 1;
+
+    function animate(timeStamp) {
+        ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+
+        game.update(deltaTime);
+        game.draw();
+
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+});
